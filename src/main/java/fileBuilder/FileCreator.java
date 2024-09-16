b7367e31c197bad1590758194587aee6512e588c
Something Something <arika.khor@gmail.com>
2024-09-16 18:15:53 -0500

    redo arch

--- src/main/java/fileBuilder/FileCreator.java
+++ src/main/java/fileBuilder/FileCreator.java
@@ -4,4 +4,5 @@ package fileBuilder;
  * Scrap pages from classNav, and make CSV file here (do not need to normalize data (if needed) here)
  */
 public class FileCreator {
+
 }
---
+++ src/main/java/standard_sch_class.java
@@ -0,0 +1,5 @@
+/**
+ * Create new std class object based off of school classes in classNav for readibility ease
+ */
+public class standard_sch_class {
+}
