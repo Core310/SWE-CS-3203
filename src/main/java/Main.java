a0fa8317d263958d8f4940c252ea69d9ef335030
Something Something <arika.khor@gmail.com>
2024-09-27 11:44:11 -0500

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,11 @@
 /**
- * Ground rules for random stuff:
- * - NEVER REBASE (ask for permission, likely I'll say no)
- * - Maximum 3 layers of abstraction!!! (VERY IMPT) If you have a reason to go deeper pls contact me (@Arika)
- * - KISS Principles (Keep it simple, stupid)
- * - Only use GPT if you're not going to create a lot of trash very quickly.
- * - Document by naming your methods/variables well
- * - Use javadoc comments to explain non-trivial methods, or just how they work
  *
  * Code layout (packages):
  * AlgoSorter: usr inp, and final output data. Will have atleast 2 classes: one to output all classes usr inp, & another 2 deal with given data.
+ *  Output
  * fileBuilder: WebScrapper, data normalizer. Obtain data from classNav and throw it into a CSV file
  * Classes: Std class templates for i/o reading (java class of a school class for ease of data reading
+ * Data format for each class object is stated in standard_sh_class object
  *
  * Eventually should return JSON format of all possible classes, from there can prettify somehow...
  */
