19b00218206997c2fe8042ca659e6509b9fd299e
Something Something <arika.khor@gmail.com>
2024-10-07 11:48:20 -0500

    update unit tests

--- src/main/java/AlgoSorter/QueryIndexer.java
+++ src/main/java/AlgoSorter/QueryIndexer.java
@@ -7,14 +7,28 @@ import java.util.ArrayList;
  * @see QueryIndexer for the actual implementation of
  * return Arraylist<Arraylist<classes>>, where classes may be string s
  */
+
 public class QueryIndexer {
     @lombok.Getter
     ArrayList<String> usr_query;
 
-
-
+    /**
+     * Input constructor for usr query
+     * @param al
+     */
     public QueryIndexer(ArrayList al) {
         al = usr_query;
+    }
+
+    /**
+     *
+     * @param query
+     * @return true if inner class userQuery arraylist has no duplicates
+     */
+    public boolean updateOneQuery(String query){
+        if (usr_query.contains(query)) return  false;
+        usr_query.add(query);
+        return true;
     }
 
 
